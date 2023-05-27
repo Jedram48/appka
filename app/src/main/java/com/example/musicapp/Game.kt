dccb3b6a79e6c7425c192178ce668d58662a2ffb
Mariia Hnatiuk <mariia.hnatiuk@nokia.com>
2023-05-27 11:11:30 +0200

    Added basic app navigation

---
+++ app/src/main/java/com/example/musicapp/Game.kt
@@ -0,0 +1,53 @@
+package com.example.musicapp
+import SecondFragment
+import ThirdFragment
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Window
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+//import kotlinx.android.synthetic.main.activity_main.*
+//import kotlinx.android.synthetic.main.game.*
+
+class Game : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE)
+        setContentView(R.layout.game)
+
+        val firstFragment=FirstFragment()
+        val secondFragment=SecondFragment()
+        val thirdFragment=ThirdFragment()
+
+        setCurrentFragment(firstFragment)
+
+        val myBottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+
+        myBottomNavigationView.setOnNavigationItemSelectedListener {item ->
+            when(item.itemId){
+                R.id.levels->{
+                    setCurrentFragment(firstFragment)
+                    print("___first fragment\n")
+                    true
+                }
+                R.id.learning->{
+                    setCurrentFragment(secondFragment)
+                    true
+                }
+                R.id.statistic->{
+                    setCurrentFragment(thirdFragment)
+                    true
+                }
+
+                else -> {false}
+            }
+        }
+    }
+
+    private fun setCurrentFragment(fragment:Fragment)=
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flFragment,fragment)
+            commit()
+        }
+}
+//TODO change color of items when chosen
--- app/src/main/java/com/example/musicapp/MainActivity.kt
+++ app/src/main/java/com/example/musicapp/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.musicapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.view.Window
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE)
         setContentView(R.layout.activity_main)
+    }
+
+    fun startGame(view: View) {
+        val intent = Intent(this, Game::class.java)
+        startActivity(intent)
     }
 }
