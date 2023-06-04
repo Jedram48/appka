0eb14eac2841eb51c4571a318d46824f0eb6cf38
Mariia Hnatiuk <mariia.hnatiuk@nokia.com>
2023-06-04 23:26:51 +0200

    Basic piano implemented

--- app/src/main/java/com/example/musicapp/EarTrainingSection.kt
+++ app/src/main/java/com/example/musicapp/EarTrainingSection.kt
@@ -6,9 +6,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
 import androidx.navigation.Navigation
+import com.citizenwarwick.music.Note
+import com.citizenwarwick.music.PitchClass
+import com.citizenwarwick.music.chord
+import com.citizenwarwick.pianoroll.PianoRoll
+import com.citizenwarwick.pianoroll.PianoRollOptions
 
 
 class EarTrainingSection : Fragment() {
@@ -21,6 +32,22 @@ class EarTrainingSection : Fragment() {
         return ComposeView(requireContext()).apply {
             setContent {
                 Text(text = "Hello world.")
+                MaterialTheme {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .fillMaxHeight(),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        PianoRoll(
+                            startNote = Note(PitchClass.C, 0),
+                            endNote = Note(PitchClass.C, 1),
+                            options = PianoRollOptions(
+                                highlightedNotes = "C0 E0 G0 B0".chord
+                            )
+                        )
+                    }
+                }
             }
         }
     }
